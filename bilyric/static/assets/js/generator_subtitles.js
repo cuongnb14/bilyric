b6f11d626e764c0af496dce0873b7ae9ef0988cf
cuongnb14 <cuongnb14@gmail.com>
2016-10-16 17:47:37 +0700

    add next pre line button

--- bilyric/static/assets/js/generator_subtitles.js
+++ bilyric/static/assets/js/generator_subtitles.js
@@ -27,6 +27,18 @@ jQuery(document).ready(function ($) {
         var lineNumber = $("#cl-go-line").val();
         $("#cl-subtitles-container").mCustomScrollbar("scrollTo", "#ps" + lineNumber);
     });
+    $("#cl-go-pre").click(function () {
+        var lineNumber = parseInt($("#cl-go-line").val());
+        lineNumber = lineNumber - 1;
+        $("#cl-go-line").val(lineNumber);
+        $("#cl-subtitles-container").mCustomScrollbar("scrollTo", "#ps" + lineNumber.toString());
+    });
+    $("#cl-go-next").click(function () {
+        var lineNumber = parseInt($("#cl-go-line").val());
+        lineNumber = lineNumber + 1;
+        $("#cl-go-line").val(lineNumber);
+        $("#cl-subtitles-container").mCustomScrollbar("scrollTo", "#ps" + lineNumber.toString());
+    });
 
     $("#cl-apply-subs").click(function () {
         var sub1 = [];
